e0ca5a3bf8e2a2b197faeebe0bb84fe0bc27a7c8
OlenaCherneta <taliara.work@gmail.com>
2023-02-04 14:49:47 +0200

    Added GitHub repository link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by Olena Cherneta and is{" "}
+        <a
+          href="https://github.com/OlenaCherneta/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
